786b2bb5cc2b265f8bfce26d3e95a011043b6804
Abdulaziz Jastanieh <asalehjastanieh@stu.kau.edu.sa>
2023-04-20 15:46:37 +0300

    yes

---
+++ src/main/java/com/mycompany/fix_it/Fix_it.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fix_it;
+
+/**
+ *
+ * @author 96657
+ */
+public class Fix_it {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
