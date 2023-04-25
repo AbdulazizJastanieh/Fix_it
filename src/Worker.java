ca28ab424350fc0c18cb2556ea842e1f561e3d91
Abdulaziz Jastanieh <asalehjastanieh@stu.kau.edu.sa>
2023-04-25 14:18:22 +0300

    "plus ultra"

--- src/Worker.java
+++ src/Worker.java
@@ -1,13 +1,61 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.util.Date;
 
-/**
- *
- * @author ASUS
- */
-public class Worker {
-    
+public class Worker extends Person{
+    private String WID;
+    private String Working_area;
+    private double Balance;
+    private Service services[];
+
+    public String getWID() {
+        return WID;
+    }
+
+    public void setWID(String WID) {
+        this.WID = WID;
+    }
+
+    public String getWorking_area() {
+        return Working_area;
+    }
+
+    public void setWorking_area(String Working_area) {
+        this.Working_area = Working_area;
+    }
+
+    public double getBalance() {
+        return Balance;
+    }
+
+    public void setBalance(double Balance) {
+        this.Balance = Balance;
+    }
+
+    public Service[] getServices() {
+        return services;
+    }
+
+    public void setServices(Service[] services) {
+        this.services = services;
+    }
+
+    public Worker(String WID, String Working_area, double Balance, Service[] services, String First_name, String last_name, int age, Date Date_of_Birth, String Username, String Password, Order[] orders) {
+        super(First_name, last_name, age, Date_of_Birth, Username, Password, orders);
+        this.WID = WID;
+        this.Working_area = Working_area;
+        this.Balance = Balance;
+        this.services = services;
+    }
+
+    public Worker(String WID, String Working_area, double Balance, Service[] services) {
+        this.WID = WID;
+        this.Working_area = Working_area;
+        this.Balance = Balance;
+        this.services = services;
+    }
+
+
+    public Worker(){
+        super();
+    }
+
 }
