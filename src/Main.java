cd6e986b963421d849e557230cf47e74345d81db
hamza <hamooz15@hotmail.com>
2023-04-25 14:51:34 +0300

    we will work on this V

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
     ArrayList<Customer> cou = new ArrayList<Customer>();
-        Connection conn= DataBase.connect();
+         DataBase.connect();
 
 
         
