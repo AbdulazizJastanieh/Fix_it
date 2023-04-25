b84d43bacd1a648196a4f9574c3e5dd021f23861
hamza <hamooz15@hotmail.com>
2023-04-25 07:45:23 +0300

    notes

--- src/main/java/com/mycompany/fix_it/Store.java
+++ src/main/java/com/mycompany/fix_it/Store.java
@@ -5,17 +5,18 @@ public class Store {
 
 
     public static void connect() {
-        String url = "jdbc:mysql://sql8.freemysqlhosting.net";
-        //String url = "sql8.freemysqlhosting.net";
-        String username = "sql8614265";
-        String password = "EMxp1rkYMb";
+        String url = "jdbc:mysql://sql8.freemysqlhosting.net"; //this is the URL with add-on of the dataBase the below is the base URL
+        //String url = "sql8.freemysqlhosting.net"; //base URL
+        String username = "sql8614265"; //USERNAME and the name of the dataBASE
+        String password = "EMxp1rkYMb"; //PASS of the database
 
         try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection conn = DriverManager.getConnection(url, username, password);
+            Class.forName("com.mysql.cj.jdbc.Driver"); //things related to the JAR file
+            Connection conn = DriverManager.getConnection(url, username, password); //making the connection
             System.out.println("connected !!!");
-            Statement s = conn.createStatement();
-            s.execute("USE sql8614265");
+            Statement s = conn.createStatement(); //create statement
+            s.execute("USE sql8614265"); //to use this database (we only have 1 database in the server)
+            s.close(); //close the Statement
         } catch(Exception e){ System.out.println(e);}
     }
 
